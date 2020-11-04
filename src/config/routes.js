b389fc7e1317430c164fdd86de83feb65aaf1486
code-ye <yezhiwei0762@163.com>
2020-11-04 17:29:16 +0800

    feat: 项目完成

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,10 +1,34 @@
-import VerifyPhone from "../pages/regist/VerifyPhone";
+import VerifyPhone from "@pages/regist/VerifyPhone";
+import VerifyCode from "@pages/regist/VerifyCode";
+import VerifyPassword from "@pages/regist/VerifyPassword";
+import CountryPicker from "@comps/CountryPicker";
+import PhoneLogin from "@pages/login/PhoneLogin";
 
 // 路由配置文件
 const routes = [
   {
     path: "/regist/verifyPhone",
     component: VerifyPhone,
+    exact: true,
+  },
+  {
+    path: "/regist/verifycode",
+    component: VerifyCode,
+    exact: true,
+  },
+  {
+    path: "/regist/verifypassword",
+    component: VerifyPassword,
+    exact: true,
+  },
+  {
+    path: "/common/countrypicker",
+    component: CountryPicker,
+    exact: true,
+  },
+  {
+    path: "/login",
+    component: PhoneLogin,
     exact: true,
   },
 ];
--- src/pages/regist/VerifyPhone/index.jsx
+++ src/pages/regist/VerifyPhone/index.jsx
@@ -3,20 +3,20 @@ import { NavBar, Icon, InputItem, WingBlank, Modal, Toast } from "antd-mobile";
 import { createForm } from "rc-form";
 import { reqVerifyPhone } from "@api/regist";
 import { reqSendCode } from "@api/login";
+
 import VerifyButton from "@comps/VerifyButton";
+import { phoneReg } from "@utils/reg";
 
 import "./index.css";
 
 class VerifyPhone extends Component {
   state = {
-    isDisabled: true,
+    isDisabled: true, // 控制按钮显示哪个
   };
 
   componentDidMount() {
     Modal.alert(
-      //弹框的标题
       "注册协议及隐私政策",
-      //弹框的内容
       <span className="policy-text">
         在您注册成为硅谷用户的过程中，您需要完成我们的注册流程并通过点击同意的形式在线签署以下协议，
         <strong className="policy-strong-text">
@@ -25,16 +25,13 @@ class VerifyPhone extends Component {
         ：<span className="policy-content">《硅谷用户注册协议》</span>
         <span className="policy-content">《硅谷隐私政策》</span>
       </span>,
-      //弹框的按钮
       [
         {
           text: "不同意",
-          //点击不同意时触发的回调
-          onPress: () => console.log("cancel"),
+          onPress: () => this.props.history.goBack(),
         },
         {
           text: "同意",
-          //可以在对象中写style
           style: { backgroundColor: "red", color: "#fff" },
         },
       ]
@@ -42,105 +39,99 @@ class VerifyPhone extends Component {
   }
 
   // 当用户输入数据时就会触发
+  // 表单验证
   validator = (rule, value, callback) => {
-    // rule：即传入的字段，为phone，  value：为表单的value
-    //每当表单改变时都会触发这个方法
     // console.log(rule, value);
-    const reg = /^(0|86|17951)?(13[0-9]|15[012356789]|166|17[3678]|18[0-9]|14[57]|199)[0-9]{8}$/;
-    //下一步按钮默认为不可点击
+
     let isDisabled = true;
 
-    if (reg.test(value)) {
-      //校验成功后，下一步设置为可点击
+    if (phoneReg.test(value)) {
       isDisabled = false;
     }
-    //更新state
+
     this.setState({
       isDisabled,
     });
+
     // callback必须调用，否则检验失败
     // callback(message) 校验失败
     // callback() 校验成功
     callback();
   };
 
-  //发送验证码
+  // 发送验证码
   sendCode = (phone) => {
-    Modal.alert(
-      "",
-      `我们将发送短信/语音验证码至：${phone}`, //弹框的按钮
-      [
-        {
-          text: "取消",
-          //点击不同意时触发的回调
-          onPress: () => console.log("cancel"),
+    Modal.alert("", `我们将发送短信/语音验证码至：${phone}`, [
+      {
+        text: "取消",
+        // onPress: () => console.log("cancel"),
+      },
+      {
+        text: "确定",
+        style: { backgroundColor: "red", color: "#fff" },
+        onPress: async () => {
+          // 发送请求 请求短信验证码
+          await reqSendCode(phone);
+
+          this.props.history.push("/regist/verifycode", phone);
         },
-        {
-          text: "确定",
-          //可以在对象中写style
-          style: { backgroundColor: "red", color: "#fff" },
-          onPress: async () => {
-            // 发送请求 请求短信验证码
-            await reqSendCode(phone);
-            //路由跳转
-            this.props.history.push("/regist/verifycode");
-          },
-        },
-      ]
-    );
+      },
+    ]);
   };
 
+  // 验证用户手机号是否注册过
   verifyPhone = async () => {
-    try {
-      // 获取单个表单项的值
-      const phone = this.props.form.getFieldValue("phone");
-      // 获取所有表单项的值
-      // const value2 = this.props.form.getFieldsValue();
-      await reqVerifyPhone(phone);
+    // 获取单个表单项的值
+    const phone = this.props.form.getFieldValue("phone");
+    // 获取所有表单项的值
+    // const value2 = this.props.form.getFieldsValue();
+    await reqVerifyPhone(phone);
 
-      // 请求成功--手机号不存在
-      // 提示弹框--确认请求短信验证码
-      console.log("success");
-      this.sendCode(phone);
-    } catch (e) {
-      if (e === "fail") return;
-      // 请求失败 - 手机号存在
-      Toast.fail(e, 3);
-      // console.log("err", e);
-    }
+    // console.log("success");
+    // 请求成功 - 手机号不存在
+    // 提示弹框 - 确认请求短信验证码
+    this.sendCode(phone);
+  };
+
+  goCountryPicker = () => {
+    this.props.history.push("/common/countrypicker", "/regist/verifyphone");
+  };
+
+  goBack = () => {
+    this.props.history.goBack();
   };
 
   render() {
     const { isDisabled } = this.state;
+    // form属性：由createForm高阶组件传递而来
     const { getFieldProps } = this.props.form;
 
+    const number = this.props.location.state || "+86";
+
     return (
-      <div>
+      <div className="verify-phone">
         <NavBar
-          // 颜色为light
           mode="light"
-          // 左侧导航
           icon={<Icon className="left" type="left" />}
-          // 点击左侧导航的回调
-          onLeftClick={() => console.log("onLeftClick")}
+          onLeftClick={this.goBack}
         >
           硅谷注册
         </NavBar>
-        {/* 两翼留白效果 */}
         <WingBlank>
           <div className="verify-phone-input">
             <InputItem
               {...getFieldProps("phone", {
-                // 表单校验规则：自定义校验规则
+                // 表单校验规则
                 rules: [{ validator: this.validator }],
               })}
-              // 表单右侧的x号
               clear
-              // 表单提示
               placeholder="请输入手机号"
             >
-              <div className="verify-phone-prefix">
-                <span>+86</span>
+              <div
+                className="verify-phone-prefix"
+                onTouchEnd={this.goCountryPicker}
+              >
+                <span>{number}</span>
                 <Icon type="down" />
               </div>
             </InputItem>
